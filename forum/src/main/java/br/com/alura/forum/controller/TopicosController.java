bd0188ac0d74c6e7e5dc328c4cdd46f1f6b880dc
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-26 19:54:29 -0300

    Usando DTO para exibir os dados

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,23 +3,22 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista() {
+	public List<TopicoDto> lista() {
 
-		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "programação"));
+		Topico topico = new Topico("Dúvidas", "Dúvida com Spring", new Curso("Spring", "programação"));
 
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 
 	}
 
