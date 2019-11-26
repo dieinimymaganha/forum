bc7b0efe2832da5b9b96af3dd1057fd14008a11f
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-26 20:40:58 -0300

    Alterando para usar o Topicorepository

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,22 +3,26 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
+
 	@RequestMapping("/topicos")
 	public List<TopicoDto> lista() {
+		List<Topico> topicos = topicoRepository.findAll();
 
-		Topico topico = new Topico("Dúvidas", "Dúvida com Spring", new Curso("Spring", "programação"));
-
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+		return TopicoDto.converter(topicos);
 
 	}
 
