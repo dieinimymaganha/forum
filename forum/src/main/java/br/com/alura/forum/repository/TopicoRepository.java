8105375efaad1ac852b81953ebbe6b586fe9a9ff
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-26 20:59:04 -0300

    Fazendo filtro por nome do curso usando String Query

--- forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,11 +1,21 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
+import org.jboss.logging.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
-	
+
+	List<Topico> findByCurso_Nome(String nomeCurso);
+
+//	Caso queria usar query
+//	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+//	List<Topico> carregaPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
+
 	
 
 }
